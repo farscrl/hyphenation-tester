49bbc9f3bca64fc8e44790b019f362a5c260bc35
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-12-03 17:55:11 +0100

    chore: update name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hyphen-tester'`, () => {
+  it(`should have as title 'hyphenation-tester'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hyphen-tester');
+    expect(app.title).toEqual('hyphenation-tester');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hyphen-tester app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hyphenation-tester app is running!');
   });
 });
